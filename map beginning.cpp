ea8b71560ecd290acd9f269fd8a8d41386ae48b5
Niloy Barua <98206229+Niloyb34@users.noreply.github.com>
2023-03-20 21:29:46 +0600

    Update map beginning.cpp

--- map beginning.cpp
+++ map beginning.cpp
@@ -15,5 +15,7 @@ int main()
     }
 /*aikane answer e one er por three asbe ,karon o>t mane o t er age ase tahole one three and two both teke age asbe lexicographically but two and three
 er modde t first character both word er same but three er h>two er w tie h age ase tie three age lexicographically two er theke tie at first one then three then two
-print hbe map er ketre ,map er ketre it++ lekha jie but it=it+1 arokom lekha jie na tie wise hlo it++ aibabe lekha */
+print hbe map er ketre ,map er ketre it++ lekha jie but it=it+1 arokom lekha jie na tie wise hlo it++ aibabe lekha 
+keyal rakbe aikane key er ascending order e mane key aikane string tie string er lexicographical order e store hbe mane oita 
+maintain hbe aikane always*/
 }
