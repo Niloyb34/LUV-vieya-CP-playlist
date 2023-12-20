4f5d2f81862dddf9ef5a2e77e84dbeca239e18b8
Niloy Barua <98206229+Niloyb34@users.noreply.github.com>
2023-12-21 01:03:13 +0600

    Update GRAPH adjacency LIST using 2D vector.cpp

--- GRAPH adjacency LIST using 2D vector.cpp
+++ GRAPH adjacency LIST using 2D vector.cpp
@@ -1,27 +1,67 @@
-//Adjacency  list representation
+//ADJACENCY LIST REPRESENTAION
 #include<bits/stdc++.h>
 using namespace std;
 int main()
 {
-    int i,v1,v2,vertices,e;
-    cout<<"Enter the vertices : ";
-    cin>>vertices;
-    vector<vector<int>>v(vertices);//5 mane 5 ti vertex ase
-    cout<<"Enter the egdes number:";
-    cin>>e;
-    for(i=0; i<e; i++)
-    {
-        cin>>v1>>v2;
-        v[v1].push_back(v2);
-        v[v2].push_back(v1);
-    }
-    for(i=0; i<v.size(); i++)
-    {
-        cout<<"Adjacency list of vertex "<<i<<" is : ";
-        for(auto t:v[i])
-        {
-            cout<<t<<" ";
-        }
-        cout<<endl;
-    }
+       int i,v1,v2,vertices,edges;
+       cout<<"Enter the number of vertices : ";
+       cin>>vertices;
+       cout<<"Enter the number of edges : ";
+       cin>>edges;
+       vector<vector<int>>v(vertices);
+       /*aikane size ta outer vector v er jnno , internal vector er kono size nie oita dyna
+       mically barbe push korar sathe sathe*/
+       cout<<"Enter the edges "<<endl;
+       for(i=0;i<edges;i++)
+       {
+              cin>>v1>>v2;
+              v[v1].push_back(v2);
+              v[v2].push_back(v1);
+              ///Creating an undirected graph
+              /*INPUTS are:
+                                   Enter the number of vertices : 5
+                Enter the number of edges : 7
+                Enter the edges
+                0 1
+                0 2
+                1 2
+                1 3
+                 2 3
+                 2 4
+                 3 4
+              */
+       }
+       for(i=0;i<vertices;i++)
+       {
+              cout<<"Adjaceny list of vertex " <<i<<"-->";
+              for(auto it:v[i])
+              {
+                     cout<<it<<" ";
+              }
+              cout<<endl;
+       }
+       /*OUTPUT WILL BE:
+       Adjaceny list of vertex 0-->1 2
+      Adjaceny list of vertex 1-->0 2 3
+      Adjaceny list of vertex 2-->0 1 3 4
+      Adjaceny list of vertex 3-->1 2 4
+      Adjaceny list of vertex 4-->2 3
+*/
 }
+/*Enter the number of vertices : 5
+Enter the number of edges : 7
+Enter the edges
+0 1
+0 2
+1 2
+1 3
+2 3
+2 4
+3 4
+Adjaceny list of vertex 0-->1 2
+Adjaceny list of vertex 1-->0 2 3
+Adjaceny list of vertex 2-->0 1 3 4
+Adjaceny list of vertex 3-->1 2 4
+Adjaceny list of vertex 4-->2 3
+*/
+
