0aefbaa0363560e4f2ce5ea9539fe59256ea6c42
Niloy Barua <98206229+Niloyb34@users.noreply.github.com>
2023-12-30 14:29:06 +0600

    Create Question 1.cpp

---
+++ Question 1.cpp
@@ -0,0 +1,26 @@
+/*
+Given N strings,print unique strings in lexicographical order
+N<=10^5
+|S|<=100000;
+mane onekgula string input hisebe asle amra just unique gula nibo and don,t need to show frequency
+tie map use na kore only set use korle hbe*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    set<string>s;
+    int n;
+    cin>>n;//how many strings
+    for(int i=0; i<n; i++)
+    {
+        string str;
+        cin>>str;
+        s.insert(str);
+    }
+    for(auto value: s)
+    {
+            cout<<value<<endl;
+    }
+
+}
